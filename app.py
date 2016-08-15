e0659cff4edff62e4e6ad8a94634551fd05255f2
unknown <WILLISR@UKPC017R0R.uk.mp.root>
2016-08-15 11:41:40 +0100

    Commiting Experiment

---
+++ app.py
@@ -0,0 +1,14 @@
+class testclass:
+    def __init__(self,_name):
+        self.MyName = _name
+    
+    def printname(self):
+        print(self.MyName)
+
+if __name__=="__main__":
+    tc = testclass("jimmy")
+    tc.printname()
+    k=input("press close to exit") 
+    tc = None
+
+
