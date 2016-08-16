c2363883ec8c411d1172a150032feace0d1524b8
unknown <WILLISR@UKPC017R0R.uk.mp.root>
2016-08-16 11:08:29 +0100

    Flask Serialization Experiment

--- app/views.py
+++ app/views.py
@@ -1,4 +1,23 @@
 from app import app
+from sqlobject import *
+from flask import jsonify
+
+    
+sqlhub.processConnection = connectionForURI('mssql://PythonUser:Experis123!@MPGSDWPSH0001:1402/PythonTest')
+
+class Person(SQLObject):
+    firstname = StringCol()
+    lastname = StringCol()
+
+    def serialize(self):  
+        return {           
+            'firstname': self.firstname, 
+            'lastname': self.lastname
+        }
+
+
+    
+
 
 @app.route('/')
 @app.route('/index')
@@ -12,5 +31,10 @@ def getin():
 @app.route('/FindMe/<user>')
 def findme(user):
     return user
+
+@app.route('/Post/<int:userid>')
+def post(userid):
+    person=Person.get(userid)
+    return jsonify(person=person.serialize())
 
 
